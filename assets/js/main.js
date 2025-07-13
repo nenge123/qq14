bcd8d53a8641b238cea615dfa59fca24d7e38370
nenge <m@nenge.net>
2025-07-14 01:00:27 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+new class {
+	constructor() {
+		const N = this;
+		Object.defineProperty(self, 'N', { get: () => N });
+		self.navigator.serviceWorker && self.navigator.serviceWorker.register('sw.js');
+		$(function () {
+			N.parse();
+			$('#layout-main').window({
+				title: '引导',
+				fit: true,
+				border: false,
+				maximized: true,
+				maximizable: false,
+				closable: false,
+				closed: false,
+				draggable: false,
+				resizable: false,
+				minimized: false,
+				minimizable: false,
+				modal: false,
+			});
+			const page = localStorage.getItem('base-page');
+			if (page) {
+				const elm = document.querySelector('[data-page="' + page + '"]');
+				if (elm instanceof HTMLElement) {
+					elm.click();
+				} else {
+					N.getPage(page);
+				}
+			}
+		})
+	}
+	/**
+	 * 
+	 * @param {Document} dom 
+	 */
+	parse(dom) {
+		dom = dom || document;
+		const N = this;
+		Array.from(dom.querySelectorAll('[data-page]'), elm => {
+			if(dom===document){
+				$(elm).prop('save',true);
+			}
+			elm.addEventListener('click', async function () {
+				const page = elm.getAttribute('data-page');
+				const newelm = document.querySelector('[page-href="'+page+'"]');
+				if(newelm instanceof HTMLElement){
+					$(newelm).window('open');
+					this.addEventListener('click',()=>$(newelm).window('open'));
+				}else{
+					N.getPage(page, this);
+				}
+			}, { once: true });
+		});
+	}
+	getPage(page, elm) {
+		const N = this;
+		const request = new XMLHttpRequest;
+		const href = new URL('pages/' + page, location);
+		request.addEventListener('readystatechange', function () {
+			if (request.readyState === request.DONE) {
+				const response = request.response;
+				const newelm = response.body.firstElementChild;
+				document.body.append(newelm);
+				$.parser.parse(newelm);
+				$(newelm).attr('page-href',page);
+				$(newelm).window({
+					fit: true,
+					border: false,
+					maximized: true,
+					maximizable: false,
+					minimized: false,
+					minimizable: false,
+					collapsed: false,
+					collapsible: false,
+					closable: true,
+					closed: false,
+					draggable: false,
+					resizable: false,
+					modal: false,
+					onOpen(){
+						if (elm) {
+							elm.addEventListener('click', function () {
+								$(newelm).window('open');
+							});
+							const save = $(elm).prop('save');
+							if (save) {
+								localStorage.setItem('base-page', elm.getAttribute('data-page'));
+							}
+						}
+					},
+					onClose(){
+						const page = localStorage.getItem('base-page');
+						if(page&&page==$(newelm).attr('page-href')){
+							localStorage.removeItem('base-page');
+						}
+					}
+				});
+			}
+		});
+		request.open('GET', href);
+		request.responseType = 'document';
+		request.send(null);
+	}
+}
---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install',
+	function (event) {
+		console.log(event.type);
+		return self.skipWaiting();
+	}
+);
+self.addEventListener('activate',
+	function (event) {
+		return self.skipWaiting();
+	}
+);
+self.addEventListener('fetch',function(event){
+	const request = event.request;
+	const url = new URL(request.url);
+	const ext = url.pathname.split('.').pop();
+	if (url.origin === location.origin) {
+		const pathname = url.pathname.replace(location.pathname,'');
+		//本域
+		switch(true){
+			case /themes\//.test(url.pathname):
+				return event.respondWith(BaseResponse(request, 'qq14-themes'));
+			break;
+		}
+	}else{
+
+	}
+
+});
+
+async function BaseResponse(request, cachename) {
+	const cache = await caches.open(cachename);
+	const response = await cache.match(request);
+	if (response instanceof Response) {
+		return response;
+	}
+	const newResponse = await fetch(request, { headers:{'cache-control':'no-cache'}});
+	if ((newResponse instanceof Response)&& newResponse.status == 200) {
+		cache.put(request, newResponse.clone());
+	}
+	return newResponse || new Response(undefined, { status: 404, statusText: 'not found' });
+}
